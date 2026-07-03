f406795684bd22f94f9f56d058e6067882cf7bc3
glowkraftee <altaf.iqbal@yahoo.com>
2026-07-03 05:28:04 +0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} /**
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./main.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
