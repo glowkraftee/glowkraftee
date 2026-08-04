a16023abdd041b833a43cc413d0979f2977a525f
glowkraftee <altaf.iqbal@yahoo.com>
2026-08-05 04:58:00 +0500

    Wrap AppRoutes with CartProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "../router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
+import { CartProvider } from "../hooks/useCart";
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </CartProvider>
     </I18nextProvider>
   );
 }
